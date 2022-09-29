cf0cd8d6ec5934fe7bab6d1492d6d56609930e3a
Allan Victor Santos <allanvs@debian-BULLSEYE-live-builder-AMD64>
2022-09-29 12:11:38 -0400

    versao fib(recursiva)

--- fib.c
+++ fib.c
@@ -1,23 +1,31 @@
 
-//fib(for)
+//fib(recursivo)
 
 #include<stdio-h>
 
-int main(){
+int fibo(intn){
 
-  int i,n,t1=0,t2=1,proxT;
-  scanf("%i",&n);
+  in(n==1){
 
-
-  for(i=1;i<=n;i++){
-    printf("%i,",t1);
-
-    proxT= t1 + t2
-
-    t1=t2
-
-    t2=proxT
+    return 0;
   }
-  return 0;
 
+  if(n==2){
+
+    return 1;
+  }
+
+  return fibo(n-1) + fibo(n-2)
+
+}
+
+int main(void){
+
+  int n;
+
+  scanf("%d", &n);
+
+  printf("%d", fibo(n));
+
+  return 0;
 }
